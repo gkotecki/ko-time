ac889e720759d98480e3f8083b8a03c5c9905c6c
gkotecki <gabrielkotecki@gmail.com>
2023-12-22 16:15:43 -0300

    ADD: initial structure and features

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,98 @@
-import { createSignal } from 'solid-js'
-import solidLogo from './assets/solid.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { For, ParentProps, createSignal } from 'solid-js'
 
-function App() {
-  const [count, setCount] = createSignal(0)
+const [savedTasks, setSavedTasks] = createSignal<any[]>(
+  JSON.parse(localStorage.getItem('tasks') || '[]'),
+)
 
+export function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={solidLogo} class="logo solid" alt="Solid logo" />
-        </a>
+      <div class="flex w-full gap-4 p-4">
+        <TaskManagement />
+        <Calendar />
       </div>
-      <h1>Vite + Solid</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count()}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Solid logos to learn more
-      </p>
     </>
   )
 }
 
-export default App
+function TaskManagement() {
+  let val = ''
+  return (
+    <div class="flex flex-col gap-4 rounded-lg border p-4">
+      <h1 class="whitespace-nowrap text-center">Task Management</h1>
+
+      <For each={savedTasks()}>
+        {item => (
+          <div class="flex gap-4">
+            <Task label={item} hours={1} />
+            <button
+              class="rounded border bg-red-600 p-1 text-white"
+              onClick={() => {
+                const tasks = savedTasks().filter(i => i !== item)
+                setSavedTasks(tasks)
+                localStorage.setItem('tasks', JSON.stringify(tasks))
+              }}>
+              Del
+            </button>
+          </div>
+        )}
+      </For>
+
+      <div class="flex gap-4">
+        <input class="flex-1 rounded border p-1" onKeyDown={e => (val = e.currentTarget.value)} />
+        <button
+          class="rounded border bg-green-600 p-1 text-white"
+          onClick={() => {
+            const tasks = [...savedTasks(), val]
+            setSavedTasks(tasks)
+            localStorage.setItem('tasks', JSON.stringify(tasks))
+          }}>
+          Add
+        </button>
+      </div>
+    </div>
+  )
+}
+
+function Calendar() {
+  const [count, setCount] = createSignal(new Array(31).fill('').map((_, i) => i + 1))
+
+  return (
+    <>
+      <div class="flex w-full flex-col gap-4 rounded-lg border p-4">
+        <h1 class="text-center">{new Date().toLocaleString(undefined, { month: 'long' })}</h1>
+        <div class="grid grid-cols-7 gap-1">
+          <For each={['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']}>
+            {item => <div class="text-center">{item}</div>}
+          </For>
+          <For each={count()}>
+            {item => (
+              <CalendarDay day={item}>
+                <Task label="aaa" />
+                <Task label="bbb" />
+              </CalendarDay>
+            )}
+          </For>
+        </div>
+      </div>
+    </>
+  )
+}
+
+function CalendarDay({ day = 0, children = null }: ParentProps<{ day: number }>) {
+  return (
+    <span class="flex flex-col items-center justify-center gap-1 rounded-md border p-1 text-sm">
+      {day}
+      {children}
+    </span>
+  )
+}
+
+function Task({ label = 'task', hours = 1 }) {
+  return (
+    <div class="flex w-full items-center justify-between rounded border bg-blue-600 px-2 py-px text-xs text-white">
+      <span>{label}</span>
+      <span>{hours}h</span>
+    </div>
+  )
+}
